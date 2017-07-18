0ecbf2a348cc10d1bbf33aae0b41ea20ff2a2270
yurimalheiros <yurimalheiros@gmail.com>
2017-07-18 10:13:50 -0300

    recupera pagamentos por períodos

--- api/pagamentos/views.py
+++ api/pagamentos/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework import mixins
@@ -58,7 +59,18 @@ class PagamentoPorServidor(mixins.ListModelMixin, viewsets.GenericViewSet):
     lookup_url_kwarg = "servidor_id"
 
     def get_queryset(self):
+        month_begin = self.request.query_params.get('mes_inicio', None)
+        month_end = self.request.query_params.get('mes_fim', None)
+        year_begin = self.request.query_params.get('ano_inicio', None)
+        year_end = self.request.query_params.get('ano_fim', None)
         servidor_id = self.kwargs.get(self.lookup_url_kwarg)
+
+        if all(v is not None for v in [month_begin, month_end, year_begin, year_end]):
+            date_begin = datetime.date(int(year_begin), int(month_begin), 1)
+            date_end = datetime.date(int(year_end), int(month_end), 1)
+
+            return FolhaMunicipio.objects.filter(id_servidor=servidor_id).filter(data_pagamento__gte=date_begin).filter(data_pagamento__lte=date_end).order_by("-valor")
+
         return FolhaMunicipio.objects.filter(id_servidor=servidor_id).order_by("-valor")
 
 
@@ -67,5 +79,18 @@ class PagamentoPorUnidadeGestora(mixins.ListModelMixin, viewsets.GenericViewSet)
     lookup_url_kwarg = "unidade_id"
 
     def get_queryset(self):
+        month_begin = self.request.query_params.get('mes_inicio', None)
+        month_end = self.request.query_params.get('mes_fim', None)
+        year_begin = self.request.query_params.get('ano_inicio', None)
+        year_end = self.request.query_params.get('ano_fim', None)
         unidade_id = self.kwargs.get(self.lookup_url_kwarg)
+
+        if all(v is not None for v in [month_begin, month_end, year_begin, year_end]):
+            date_begin = datetime.date(int(year_begin), int(month_begin), 1)
+            date_end = datetime.date(int(year_end), int(month_end), 1)
+
+            return FolhaMunicipio.objects.filter(id_unidade_gestora=unidade_id).filter(data_pagamento__gte=date_begin).filter(data_pagamento__lte=date_end).order_by("-valor")
+
         return FolhaMunicipio.objects.filter(id_unidade_gestora=unidade_id).order_by("-valor")
+
+
