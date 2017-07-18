12f32f4c02587dfa3294ce5baf6d1b95193daf27
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2017-07-18 20:26:45 -0300

    adicionada configuracao pra o banco mysql

--- api/core/settings.py
+++ api/core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'folha_limpa',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
